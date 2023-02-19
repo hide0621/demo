db73b5a53c8fcad8591fc7307688fbe8f67e7788
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-20 08:59:24 +0900

    Thymeleafを使用してHello Worldを出力した

--- src/main/kotlin/com/example/hello_app/demo/Hello.kt
+++ src/main/kotlin/com/example/hello_app/demo/Hello.kt
@@ -1,13 +1,15 @@
 package com.example.hello_app.demo
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class Hello {
 
     @RequestMapping("/")
-    fun index(): String {
-        return "Hello World!"
+    fun index(model: Model): String {
+        model.addAttribute("Greeting", "Hello World!")
+        return "index"
     }
 }
